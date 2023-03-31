11583de2632adc713c0a9d9b77762bd299358b01
KiritoVV <113103472+KiritoVV@users.noreply.github.com>
2023-03-31 15:23:32 +0100

    i have added a Inventory UI

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject InvetoryMenu;
+    private bool menuActivated;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Inventory") && menuActivated)
+        {
+            Time.timeScale = 1;
+            InvetoryMenu.SetActive(false);
+            menuActivated = false;
+        }
+
+        else if (Input.GetButtonDown("Inventory") && !menuActivated)
+        {
+            Time.timeScale = 0;
+            InvetoryMenu.SetActive(true);
+            menuActivated = true;
+        }
+    }
+}
